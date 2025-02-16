868c6675f3c4f3f1b6851b6212349deb280797f2
AakashFrexy <as4938@njit.edu>
2025-02-16 18:33:38 -0500

    changed test_operations wrt conftest

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,41 +1,22 @@
 """
 Parametrized tests for the Operations class.
 """
-
-import pytest
 from app.operations import Operations  # Adjust this import if needed
 
-def test_addition(a, b, operation, expected):
+def test_operations(a, b, operation, expected):
     """
-    Test that addition of a and b returns the expected sum.
+    A single test function that receives random operation
+    from conftest and calls the correct operation method to compare with
+    expected
     """
-    result = Operations.addition(a, b)
+    if operation is Operations.addition:
+        result = Operations.addition(a,b)
+    elif operation is Operations.subtraction:
+        result = Operations.subtraction(a,b)
+    elif operation is Operations.multiplication:
+        result = Operations.multiplication(a,b)
+    elif operation is Operations.division:
+        result = Operations.division(a,b)
+    else:
+        raise ValueError(f"unknown operation {operation}")
     assert result == expected
-
-def test_subtraction(a, b, operation, expected):
-    """
-    Test that subtraction of b from a returns the expected difference.
-    """
-    result = Operations.subtraction(a, b)
-    assert result == expected
-
-def test_multiplication(a, b, operation, expected):
-    """
-    Test that multiplication of a and b returns the expected product.
-    """
-    result = Operations.multiplication(a, b)
-    assert result == expected
-
-def test_division(a, b, operation, expected):
-    """
-    Test that division of a by b returns the expected quotient.
-    """
-    result = Operations.division(a, b)
-    assert result == expected
-
-def test_division_by_zero():
-    """
-    Test that dividing by zero raises a ValueError with the correct message.
-    """
-    with pytest.raises(ValueError, match="Division By zero is not allowed."):
-        Operations.division(5, 0)
